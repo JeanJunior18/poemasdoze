a98e9c915e58153d423eadba4507e350cecfca87
Jean Junior <jclgjunior@gmail.com>
2020-05-07 11:42:53 -0300

    Core aplications ok

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -4,21 +4,27 @@ import { Link } from 'react-router-dom'
 import './style.css';
 
 import Header from '../../components/header/index'
+// import Footer from '../../components/footer/index'
 
 export default function Home() {
   return (
     <div>  
       <Header/>
 
-      Home
-{/* 
-      <Link to='/main'>
-        <button>baixar Livretos</button>
-      </Link>
-      <Link to='/login'>
-        <button>Sign in</button>
-      </Link> */}
+      <div className="container">
+         <h1>Seja bem vindo ao Poemas do Zé</h1>
+         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores, nesciunt? Animi repellendus dicta neque, quas totam atque accusantium recusandae nulla voluptatum vel? Impedit, molestias dolor? Consectetur officiis laborum obcaecati dolore.</p>
+         <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nobis non ea, magni harum libero ipsa modi ducimus eos deserunt commodi eveniet corporis sunt, odit culpa, laborum quasi esse consequatur id.</p>
+      </div>
+      <div className="container"> 
+        <h3>Fique a vontade para baixar os livretos</h3>
+        <Link to='/main'><button>Clique aqui para baixar!</button></Link>
+        <h3>Você também pode publicar gratuitamente seus livretos</h3>
+        <Link to='/register'><button>Clique aqui para começar a publicar</button></Link>
+      </div>
 
+      
+      {/* <Footer /> */}
     </div>
   );
 }
--- src/pages/Main/main.js
+++ src/pages/Main/main.js
@@ -1,71 +1,85 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import {useHistory} from 'react-router-dom';
+import req from '../../api/axios'
 
 import './style.css';
+import { useEffect } from 'react';
+import { useState } from 'react';
 
-import  Header from '../../components/header/index';
 
 export default function Main() {
 
-    const booklests = [
-        {
-            title: 'Tudo que eu não disse',
-            author: 'Zé Lucas',
-            imgLink: 'http://poemasdoze.epizy.com/Livreto_tudo%20o%20que%20eu%20n%C3%A3o%20disse.png',
-            downloadLink: 'https://bit.ly/download_tudooqueeunaodisse'
-        },
-        {
-            title: 'Cordel da roça encantanda',
-            author: 'Zé Lucas',
-            imgLink: 'http://poemasdoze.epizy.com/Cordel_da_roca_encantada.png',
-            downloadLink: 'https://bit.ly/download_rocaencantada'
+
+  const [booklets, setBooklets] = useState([])
+
+  async function loadBooks(){
+    const response = await req.get('booklets')
+    console.log(response.data)
+    setBooklets(response.data)
+  }
+  
+
+  useEffect(()=>{loadBooks()},[])
+    // const booklests = [
+    //     {
+    //         title: 'Tudo que eu não disse',
+    //         author: 'Zé Lucas',
+    //         imgLink: 'http://poemasdoze.epizy.com/Livreto_tudo%20o%20que%20eu%20n%C3%A3o%20disse.png',
+    //         downloadLink: 'https://bit.ly/download_tudooqueeunaodisse'
+    //     },
+    //     {
+    //         title: 'Cordel da roça encantanda',
+    //         author: 'Zé Lucas',
+    //         imgLink: 'http://poemasdoze.epizy.com/Cordel_da_roca_encantada.png',
+    //         downloadLink: 'https://bit.ly/download_rocaencantada'
             
-        },
-        {
-          title: 'Cordel da roça encantanda',
-          author: 'Zé Lucas',
-          imgLink: 'http://poemasdoze.epizy.com/Cordel_da_roca_encantada.png',
-          downloadLink: 'https://bit.ly/download_rocaencantada'
+    //     },
+    //     {
+    //       title: 'Cordel da roça encantanda',
+    //       author: 'Zé Lucas',
+    //       imgLink: 'http://poemasdoze.epizy.com/Cordel_da_roca_encantada.png',
+    //       downloadLink: 'https://bit.ly/download_rocaencantada'
           
-      },
-      {
-        title: 'Cordel da roça encantanda',
-        author: 'Zé Lucas',
-        imgLink: 'http://poemasdoze.epizy.com/Cordel_da_roca_encantada.png',
-        downloadLink: 'https://bit.ly/download_rocaencantada'
+    //   },
+    //   {
+    //     title: 'Cordel da roça encantanda',
+    //     author: 'Zé Lucas',
+    //     imgLink: 'http://poemasdoze.epizy.com/Cordel_da_roca_encantada.png',
+    //     downloadLink: 'https://bit.ly/download_rocaencantada'
         
-      },
-      {
-        title: 'Cordel da roça encantanda',
-        author: 'Zé Lucas',
-        imgLink: 'http://poemasdoze.epizy.com/Cordel_da_roca_encantada.png',
-        downloadLink: 'https://bit.ly/download_rocaencantada'
+    //   },
+    //   {
+    //     title: 'Cordel da roça encantanda',
+    //     author: 'Zé Lucas',
+    //     imgLink: 'http://poemasdoze.epizy.com/Cordel_da_roca_encantada.png',
+    //     downloadLink: 'https://bit.ly/download_rocaencantada'
         
-      },
-      {
-        title: 'Cordel da roça encantanda',
-        author: 'Zé Lucas',
-        imgLink: 'http://poemasdoze.epizy.com/Cordel_da_roca_encantada.png',
-        downloadLink: 'https://bit.ly/download_rocaencantada'
+    //   },
+    //   {
+    //     title: 'Cordel da roça encantanda',
+    //     author: 'Zé Lucas',
+    //     imgLink: 'http://poemasdoze.epizy.com/Cordel_da_roca_encantada.png',
+    //     downloadLink: 'https://bit.ly/download_rocaencantada'
         
-      },
+    //   },
     
-    ];
+    // ];
 
     
 
-    const user = localStorage.getItem('user')
-    const history = useHistory();
-    function logout(){
-      localStorage.clear()
-      history.push('/')
-    }
+  const user = localStorage.getItem('user')
+  const history = useHistory();
+  function logout(){
+    localStorage.clear()
+    history.push('/')
+  }
   return (
 		<div>
 			<div className="content">
-        {booklests.map(book=>(<div key={book.title}>
-          <img src={book.imgLink} alt="Capa do Livro"/>
-        <button className="download"><p>"{book.title}"</p><p>{book.author}</p></button>
+        {booklets.map(book=>(
+        <div className='card' key={book.id} onClick={(()=>{window.location.href = book.downloadlink})}>
+          <img src={book.imageLink} alt="Capa do Livro"/>
+        <div className="download"><p>"{book.title}"</p><p>{book.author}</p></div>
         </div>))}
       </div>
 
